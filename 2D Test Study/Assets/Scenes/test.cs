87bfd8d1b6b57495976c9c6fedcb55e9aa22bb04
Nick-27-04 <scy042778@naver.com>
2025-12-15 15:36:22 +0900

    01FirstUpload
    처음임

---
+++ 2D Test Study/Assets/Scenes/test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject go 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate
+    }
+}
