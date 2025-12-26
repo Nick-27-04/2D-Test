f7e71c047aeff46032b8bd8d52b39ac9d0fab9c6
Nick-27-04 <scy042778@naver.com>
2025-12-26 10:16:16 +0900

    apple
    추가본

--- 스티브 잡스 동냥/Assets/02Scripts/GameDirector.cs
+++ 스티브 잡스 동냥/Assets/02Scripts/GameDirector.cs
@@ -11,19 +11,43 @@ public class GameDirector : MonoBehaviour
     public float time = 60f;
     public int point = 0;
 
+    itemGenerator itemGenerator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timeText = GameObject.Find("Time txt").GetComponent <Text>();
         scoreText = GameObject.Find("Score txt ").GetComponent<Text>();
-
+        itemGenerator = GetComponent<itemGenerator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         time -= Time.deltaTime;
+        if (time < 0) 
+        {
+           time = 0;
+            itemGenerator.SetParameter(100000f, 0, 0);
+        
+        } 
+        else if (0 <= this.time && this.time <= 5f)
+        {
+            itemGenerator.SetParameter(0.7f, -4f, 3);
+        }
+        else if(5<= this.time && this.time <= 10)
+        {
+            itemGenerator.SetParameter(0.8f, -3f, 4);
+
+        }
+        else if (10 <= this.time&& this.time <=20)
+        {
+            itemGenerator.SetParameter(1f, -3f, 2);
+
+        }
+
+
         timeText.text = "TIme:" + time.ToString("F1");
         scoreText.text = "Score:" + point.ToString(); 
 
